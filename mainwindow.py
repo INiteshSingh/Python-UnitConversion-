f44ac70faa2f9fa97e111de544d2b96b9c16fa99
INiteshSingh <niteshsingh984858@gmail.com>
2025-03-15 11:22:36 +0530

    Added Date and time functionality for the gui window

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,24 @@
 import tkinter as tk
+import datetime as dt
+try:
+    window = tk.Tk()   
+    window.geometry("800x500")
+    window.title("Goods Tracker")
+except:
+    print("Something Went Wrong With Generating Tkinter Gui")
 
-window = tk.Tk()
-window.geometry("800x500")
-
+try:
 #Label for the heading of the window
-label = tk.Label(master=window,text="Goods Tracker with Date and Amount of Goods Bought for Store",foreground="black",background="green")
-label.pack()
+    label = tk.Label(master=window,text="Goods Tracker with Date and Amount of Goods Bought for Store"
+                     ,foreground="white",background="#ba71ee")
+    label.pack()
+#Label to display the current date and time in the GUI
+    dateTime = dt.datetime.now() #datetime variable to get the current date and time when the gui is opened
+    datetimeLable = tk.Label(window,text=dateTime,font=("Arial",10),foreground="green",background="black")    
+    datetimeLable.palce(x=20,y=20)
+except:
+    print("Something Went Wrong With Tkinter label Generation")
+
 window.mainloop()
+
+print("Tkinter Window Closed")
